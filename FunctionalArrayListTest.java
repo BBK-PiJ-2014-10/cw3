56881b74dbf3f1d3a7d21bacaa552d6f142e3b22
Konrad Korzel <konrad.korzel@gmail.com>
2015-02-15 17:14:42 +0000

    Add tests for functional array list

---
+++ FunctionalArrayListTest.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FunctionalArrayListTest {
+
+    @Test
+    public void testHead() {
+        FunctionalArrayList fal = new FunctionalArrayList();
+        fal.add(1);
+        assertEquals(1, fal.size());
+        assertTrue(!fal.head().hasError());
+        assertEquals(1, fal.head().getReturnValue());
+    }
+
+    @Test
+    public void testRest() {
+        FunctionalArrayList fal = new FunctionalArrayList();
+        FunctionalArrayList restFal = fal.rest();
+        assertTrue(restFal.isEmpty());
+        fal.add(1);
+        assertEquals(1, fal.size());
+        restFal = fal.rest();
+        assertTrue(restFal.isEmpty());
+        fal.add(1);
+        assertEquals(2, fal.size());
+        restFal = fal.rest();
+        assertTrue(!restFal.isEmpty());
+        assertEquals(1, restFal.get(0).getReturnValue());
+    }
+
+    @Test
+    public void testSetArray() {
+        FunctionalArrayList fal = new FunctionalArrayList();
+        ReturnObjectImpl[] testArray = new ReturnObjectImpl[2];
+        fal.setArray(testArray);
+        assertTrue(fal.array == testArray);
+
+    }
+}
